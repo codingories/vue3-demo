85f4158284967f262145172fc162c27787a13ed2
朱骏杰 <zhujunjie@sundear.com>
2023-01-29 11:38:25 +0800

    requestAnimationFrame实现指定加载顺序延迟加载的demo

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import HeavyComp from "./components/heavyComp.vue";
+import {useDefer} from './hooks/useDefer'
+const defer = useDefer();
+</script>
+
+<template>
+  <div class="container">
+    <div v-for="n in 100">
+<!--      v-if="defer(n)"-->
+<!--      v-if="defer(100)"-->
+      <HeavyComp v-if="defer(n)"></HeavyComp>
+    </div>
+
+<!--    目前画的次数有没有大于一次，没有就是false 实现依次绘制-->
+<!--    <HeavyComp v-if="defer(100)"></HeavyComp>-->
+<!--    <HeavyComp v-if="defer(200)"></HeavyComp>-->
+<!--    <HeavyComp v-if="defer(300)"></HeavyComp>-->
+
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  border: 1px solid red;
+  grid-gap: 1em;
+}
+</style>
---
+++ src/components/heavyComp.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="item-container">
+    <div v-for="n in 4000" class="item">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style>
+.item-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  border: 1px solid yellowgreen;
+}
+.item {
+  width: 5px;
+  height: 3px;
+  background-color: #ccc;
+  margin: .1em;
+}
+</style>
